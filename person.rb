aa44e65c4e196fd4b4d87201cca0dbfdfc88888d
marurevi <maryrev@hotmail.com>
2022-07-18 17:42:07 -0300

    Create Person class

---
+++ person.rb
@@ -0,0 +1,26 @@
+# Create class Person
+class Person
+  initialize(name = 'Unknown', age, parent_permission = true)
+  @id = Random.new.rand(1..1000)
+  @name = name
+  @age = age
+  @parent_permission = parent_permission
+
+  # accessor get methods
+  attr_reader :id
+
+  # accessor set methods
+  attr_accessor :name, :age, :parent_permission
+
+  # private method
+  def is_of_age?
+    @age >= 18
+  end
+
+  private :is_of_age?
+
+  # public method
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
+end
