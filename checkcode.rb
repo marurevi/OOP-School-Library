5b5bed44ed1f9c9d6bf58a5dc2cee8110e899bd3
marurevi <maryrev@hotmail.com>
2022-07-19 20:44:45 -0300

    Uncomment code

--- checkcode.rb
+++ checkcode.rb
@@ -1,12 +1,10 @@
-require_relative './nameable'
 require_relative './person'
-require_relative './decorator'
 require_relative './capitalize'
 require_relative './trimmer'
 
 person = Person.new(22, 'maximilianus')
 person.correct_name
-# capitalizedPerson = CapitalizeDecorator.new(person)
-# capitalizedPerson.correct_name
-# capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-# capitalizedTrimmedPerson.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_person.correct_name
+capitalizedTrimmed_person = TrimmerDecorator.new(capitalized_person)
+capitalizedTrimmed_person.correct_name
