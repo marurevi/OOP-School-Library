1630c8ce0b67fea0bf68b543db80abf542e3295c
marurevi <maryrev@hotmail.com>
2022-07-20 17:11:06 -0300

    Create the has-many side of book and Rental

--- book.rb
+++ book.rb
@@ -1,8 +1,17 @@
 class Book 
   attr_accessor :title, :author
-  
+  attr_reader :rentals
+
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals ||= []
+  end
+
+  def add_rental(date, book, person)
+    @rental = Rental.new(date, book, person)
+    @rentals.push(rental)
+    @rental.student = person
+    @rentals.book = book
   end
 end
