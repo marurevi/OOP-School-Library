e5b258e1bd6ac4333a7e1c919534691cabb26df5
marurevi <maryrev@hotmail.com>
2022-07-26 12:40:16 -0300

    Create book menu file, class and method display

--- library/create_person_menu.rb
+++ library/create_person_menu.rb
@@ -2,8 +2,10 @@ require_relative './menu'
 
 class CreatePersonMenu < Menu
   def initialize(app)
+    super()
     @app = app
   end
+
   def display
     puts 'Do you want to create a student [1] or a teacher [2]?'
     choice = gets.chomp.to_i
@@ -12,22 +14,23 @@ class CreatePersonMenu < Menu
     puts 'Enter age:'
     age = gets.chomp.to_i
     case choice
-      when 1 
-        puts 'Enter parent permission (yes [1] /no [2]):'
-        permission = gets.chomp.to_i
-        @app.create_person(choice, age, name, parent_permission: parent_permission?(permission))
-      when 2
-        puts 'Enter specialization:'
-        specialization = gets.capitalize.chomp
-        @app.create_person(choice, age, name, specialization: specialization)
-      else
-        puts 'Invalid choice, please try again'
+    when 1
+      puts 'Enter parent permission (yes [1] /no [2]):'
+      permission = gets.chomp.to_i
+      @app.create_person(choice, age, name, parent_permission: parent_permission?(permission))
+    when 2
+      puts 'Enter specialization:'
+      specialization = gets.capitalize.chomp
+      @app.create_person(choice, age, name, specialization: specialization)
+    else
+      puts 'Invalid choice, please try again'
     end
   end
+
   private
-  
+
   def parent_permission?(permission)
-    true if permission == 1 
-    false if permission == 2  
+    true if permission == 1
+    false if permission == 2
   end
 end
