5f1c56a8a67e5a370afe0cb71fcc701e2138d4b7
marurevi <maryrev@hotmail.com>
2022-07-26 13:33:09 -0300

    Create list rentals by id menu, class and method display

---
+++ library/menu/list_rentals_menu.rb
@@ -0,0 +1,23 @@
+require_relative './menu'
+
+class ListRentalsMenu < Menu
+  def initialize(app)
+    super()
+    @app = app
+  end
+
+  def display
+    @app.list_people
+    puts 'Enter person id:'
+    id = gets.chomp.to_i
+    person = @app.people.find { |prson| prson.id == id }
+    if person.nil?
+      puts "Person with id: #{id} not found, please try again"
+    elsif person.rentals.empty?
+      puts "No rentals for #{person.name}"
+    else
+      puts "Rentals for #{person.name}:"
+      @app.list_rentals(person)
+    end
+  end
+end
